227b963e73f495524533e4b5fe8b5155cac4003d
mwillow1 <158845194+mwillow1@users.noreply.github.com>
2024-02-18 13:51:34 -0500

    Module 2 Part 1 files

--- API.py
+++ API.py
@@ -1,49 +1,23 @@
-import psycopg2
+import pg8000
 
 class databaseAPI:
-    def __init__(self, username, password):
-        self.conn = psycopg2.connect(
+    def __init__(self):
+        self.conn = pg8000.connect(
             database="postgres",
-            user=username,
-            password=password,
+            user="postgres",
+            password="password",
             host="localhost",
             port=5432
         )
 
     def get_rows(self, table_name):
-        try:
-            cur = self.conn.cursor()
-            cur.execute(f"SELECT COUNT(*) FROM {table_name}")
-            rows = cur.fetchone()[0]
-            return rows
-        except psycopg2.errors.InsufficientPrivilege:
-            return None
-        except Exception as e:
-            print(e)
-            return None
+        cur = self.conn.cursor()
+        cur.execute(f"SELECT COUNT(*) FROM {table_name}")
+        rows = cur.fetchone()[0]
+        return rows
 
     def get_names(self, table_name):
-        try:
-            cur = self.conn.cursor()
-            cur.execute(f"SELECT first_name, last_name FROM {table_name}")
-            names = cur.fetchall()
-            return names
-        except psycopg2.errors.InsufficientPrivilege:
-            return None
-        except Exception as e:
-            print(e)
-            return None
-
-    def check_credentials(self, username, password):
-        try:
-            conn = psycopg2.connect(
-                database="postgres",
-                user=username,
-                password=password,
-                host="localhost",
-                port=5432
-            )
-            conn.close()
-            return True
-        except:
-            return False
+        cur = self.conn.cursor()
+        cur.execute(f"SELECT first_name, last_name FROM {table_name}")
+        names = cur.fetchall()
+        return names
